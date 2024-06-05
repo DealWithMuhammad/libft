a9713155b5499de2bcb395a3ff2471d7da17a316
Muhammad Ahmad <muhahmad@u81z08s04.42kl.edu.my>
2024-06-05 12:42:11 +0800

    feat: add isalpha

---
+++ ft_isdigit.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muhahmad <muhahmad@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/04 14:50:44 by muhahmad          #+#    #+#             */
+/*   Updated: 2024/06/04 17:49:52 by muhahmad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <ctype.h>
+
+int	ft_isdigit(char c)
+{
+	if (c < 48 || c > 57)
+		return (0);
+	return (1);
+}
+/*int main ()
+{
+	char c = '1';
+	printf("is it digit?: %d\n", ft_isdigit(c));
+	printf("is it digit??? : %d\n", isdigit(c));
+}
+*/
