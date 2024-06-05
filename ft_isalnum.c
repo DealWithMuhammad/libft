53283eba7a37c8e82f2bc99f99421728a79f5edb
Muhammad Ahmad <muhahmad@u81z08s04.42kl.edu.my>
2024-06-05 12:54:58 +0800

    feat: updated code of isalnum

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,18 +6,27 @@
 /*   By: muhahmad <muhahmad@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 17:40:53 by muhahmad          #+#    #+#             */
-/*   Updated: 2024/06/04 17:50:47 by muhahmad         ###   ########.fr       */
+/*   Updated: 2024/06/05 12:53:45 by muhahmad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <ctype.h>
 
-int ft_isalnum(char c)
+int	ft_isalnum(char c)
 {
-	if ((c < 48 || c > 57) &&
-	(c < 65 || c > 90) &&
-	  (c < 97 || c > 122))
+	if ((c < 48 || c > 57)
+		&& (c < 65 || c > 90)
+		&& (c < 97 || c > 122))
 		return (0);
-	return (1)
+	return (1);
 }
+/*
+int main()
+{
+	char c = '&';
+	printf("is it numeric or alpha?: %d\n", ft_isalnum(c));
+	printf("is it alpha or numeric: %d", isalnum(c));
+	
+}
+*/
