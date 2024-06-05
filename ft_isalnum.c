9edd86355f7acc81fd0672f6c322a507a1395f23
Muhammad Ahmad <muhahmad@u81z08s04.42kl.edu.my>
2024-06-05 14:46:09 +0800

    added isalnum file

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,25 +6,23 @@
 /*   By: muhahmad <muhahmad@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 17:40:53 by muhahmad          #+#    #+#             */
-/*   Updated: 2024/06/05 13:07:07 by muhahmad         ###   ########.fr       */
+/*   Updated: 2024/06/05 14:44:01 by muhahmad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <ctype.h>
+#include "libft.h"
 
 int	ft_isalnum(int c)
 {
-	if ((c < 48 || c > 57)
-		&& (c < 65 || c > 90)
-		&& (c < 97 || c > 122))
-		return (0);
-	return (1);
+	return (ft_isalpha(c) || ft_isdigit(c));
 }
+
 /*
 int main()
 {
-	char c = '2';
+	char c = 'a';
 	printf("is it numeric or alpha?: %d\n", ft_isalnum(c));
 	printf("is it alpha or numeric: %d", isalnum(c));
 	
