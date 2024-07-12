352d92dfb59c1525f623a698933e8022a7250d96
Muhammad Ahmad <muhammad.ahmad90@icloud.com>
2024-07-12 16:33:37 +0800

    feat: add atoi function

---
+++ ft_atoi.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muhahmad <muhahmad@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/12 15:29:03 by muhahmad          #+#    #+#             */
+/*   Updated: 2024/07/12 16:29:50 by muhahmad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	res;
+	int	sign;
+
+	i = 0;
+	res = 0;
+	sign = 1;
+	while (str[i] == ' ')
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + str[i] - '0';
+		i++;
+	}
+	return (res * sign);
+}
+/*
+#include <stdio.h>
+int main ()
+{
+	char test[] = "--123abhb";
+	int result = ft_atoi (test);
+	printf("%d", result);
+}
+*/
