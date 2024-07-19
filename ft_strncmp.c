f86fa46952144444c82d9bef5609d5cf0e6e0694
Muhammad Ahmad <muhammad.ahmad90@icloud.com>
2024-07-19 11:52:11 +0800

    feat: add strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,36 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: muhahmad <muhahmad@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/08 13:49:01 by muhahmad          #+#    #+#             */
-/*   Updated: 2024/07/19 11:51:59 by muhahmad         ###   ########.fr       */
+/*   Created: 2024/07/13 13:57:22 by muhahmad          #+#    #+#             */
+/*   Updated: 2024/07/13 15:16:34 by muhahmad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *b, int c, size_t len)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned char	*a;
+	unsigned int	i;
 
-	a = b;
-	while (len--)
-		*a++ = c;
-	return (b);
+	i = 0;
+	while (s1[i] && s2[i] && i < n)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (s1[i] - s2[i]);
 }
 /*
+#include <string.h>
 #include <stdio.h>
+
+
 int main()
 {
-	char first[10];
-	ft_memset(first, 'A', 10);
+	char str1[] = "Hello My name is Ahmad";
+	char str2[] = "Hello My name is Muhammad";
 
-	int i = 0;
+	int result = strncmp(str1, str2, 21);
+	int my_result = ft_strncmp(str1, str2, 21);
 
-	while (i < 10)
-	printf("%c", first[i++]);
+
+	printf("%d", result);
+	printf("\n%d", my_result);
 }
 */
