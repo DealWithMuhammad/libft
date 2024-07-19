8f0ccfe8ad9159cb9f739fd053ac249f211b4d1a
Muhammad Ahmad <muhammad.ahmad90@icloud.com>
2024-07-19 15:33:59 +0800

    feat: added function in libft

--- libft.h
+++ libft.h
@@ -6,11 +6,59 @@
 /*   By: muhahmad <muhahmad@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 13:45:10 by muhahmad          #+#    #+#             */
-/*   Updated: 2024/06/08 14:43:37 by muhahmad         ###   ########.fr       */
+/*   Updated: 2024/07/19 14:05:12 by muhahmad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+// #include <unistd.h>
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
+// int	ft_isalpha(int c);
+// int	ft_isdigit(int c);
+
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <string.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+# define IS_SPACE(x) (x == ' ' || x == '\t' || x == '\r' || x == '\f')
+
+
+int					ft_atoi(const char *str);
+
+
+void				*ft_memset(void *b, int c, size_t len);
+
+
+size_t				ft_strlen(const char *str);
+char				*ft_strcpy(char *dst, const char *src);
+char				*ft_strchr(const char *s, int c);
+char                 *ft_strrchr(const char *s, int c);
+int					ft_strncmp(const char *s1, const char *s2, size_t n);
+
+int					ft_isalpha(int c);
+int					ft_isdigit(int c);
+int					ft_isalnum(int c);
+int					ft_isascii(int c);
+int					ft_isprint(int c);
+int					ft_toupper(int c);
+int					ft_tolower(int c);
+void                ft_bzero(void *s, size_t n);
+
+typedef struct		s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}					t_list;
+
+t_list				*ft_lstnew(const void *content, size_t content_size);
+void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
+void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void				ft_lstadd(t_list **alst, t_list *n);
+void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
+t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+
+
+#endif
