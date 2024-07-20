92430aa2fdd84f180003d001d3f21c52f5dab329
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-07-20 12:39:58 +0800

    fixed the norminate

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,26 +6,27 @@
 /*   By: muhahmad <muhahmad@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 15:40:59 by muhahmad          #+#    #+#             */
-/*   Updated: 2024/07/20 12:28:53 by muhahmad         ###   ########.fr       */
+/*   Updated: 2024/07/20 12:38:50 by muhahmad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "libft.h"
 
-void *ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t i;
+	size_t		i;
+	char		*d;
+	const char	*s;
 
 	i = 0;
-
-	char *d = (char *) dst;
-	const char *s = (const char *)src;
+	d = (char *) dst;
+	s = (const char *)src;
 	while (i < n)
 	{
 		d[i] = s[i];
 		i++;
 	}
-	return dst;
+	return (dst);
 }
 /*
 #include <stdio.h>
