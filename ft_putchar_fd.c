154082b533f5c20aa62e9ac8189c2e693654f8b5
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-08-03 14:45:04 +0800

    feat: add ft_putchar_fd

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,13 +6,13 @@
 /*   By: muhahmad <muhahmad@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 17:21:46 by muhahmad          #+#    #+#             */
-/*   Updated: 2024/07/29 17:41:40 by muhahmad         ###   ########.fr       */
+/*   Updated: 2024/08/03 12:32:53 by muhahmad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_putchar_fd(char c, int fd)
+void	ft_putchar_fd(char c, int fd)
 {
-    write(fd, &c, 1);
+	write(fd, &c, 1);
 }
