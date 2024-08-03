14fac12e0a618d7b02d43bc16e15aaa700ba8127
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-08-03 14:45:19 +0800

    add ft_putendl_fd.c

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,21 +6,14 @@
 /*   By: muhahmad <muhahmad@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 08:39:32 by muhahmad          #+#    #+#             */
-/*   Updated: 2024/07/30 08:39:32 by muhahmad         ###   ########.fr       */
+/*   Updated: 2024/08/03 12:32:21 by muhahmad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_putendl_fd(char *s, int fd)
+void	ft_putendl_fd(char *s, int fd)
 {
-    int i;
-    i = 0;
-
-    while (s[i] != '\0')
-    {
-        i++;
-    }
-    write(fd, &s, i);
-    write(1, "\n", 1);
+	write(fd, s, ft_strlen(s));
+	write(fd, "\n", 1);
 }
