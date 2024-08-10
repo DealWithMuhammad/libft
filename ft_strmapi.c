46c2d574966e89f44f1a321fe3cbacec486f20cb
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-08-10 14:28:29 +0800

    feat: add strmapi function

---
+++ ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muhahmad <muhahmad@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/10 13:11:40 by muhahmad          #+#    #+#             */
+/*   Updated: 2024/08/10 14:28:09 by muhahmad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t			len;
+	unsigned int	i;
+	char			*new_str;
+
+	if (s == NULL || f == NULL)
+		return (NULL);
+	len = ft_strlen(s);
+	i = 0;
+	new_str = (char *)malloc(sizeof(char) * (len + 1));
+	while (i < len)
+	{
+		new_str[i] = f(i, s[i]);
+		i++;
+	}
+	new_str[len] = '\0';
+	return (new_str);
+}
